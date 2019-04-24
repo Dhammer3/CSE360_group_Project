b7a0fbec25596fcf1e7ded1f53343904ecdcf067
Daren Hammer (Student) <dhammer4@sundevils.asu.edu>
2019-04-24 10:16:31 -0700

    updated toDoList.jav and changed it to a linked list from java lib

--- toDoList.java
+++ toDoList.java
@@ -5,7 +5,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
-import java.io.Writer;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 
@@ -13,144 +13,103 @@ import java.util.Scanner;
 public class toDoList {
 
 	int numberOfItems;
-	toDoItem head;
-	toDoItem sorted;
-
-	//toDoItem tail;
+	LinkedList<toDoItem> list = new LinkedList<toDoItem>();
+	
 	public toDoList()
 	{
 		int numberOfItems=0;
-		//this.next=null;
-		head=null;
-		sorted=null;
-
 	}
+  
 	public void deleteItem(String jobDesc)
 	{
-		toDoItem n=head;
-		int counter=0;
-		while(n.next!=null)
-		{
-			
-			n=n.next;
-			counter++;
-		}
+            for(int i=0; i<numberOfItems; i++)
+            {
+                if(list.get(i).getjobDesc()==jobDesc)
+                {
+                    list.remove(i);
+                    numberOfItems-=1;
+                    System.out.println("removed and item.");
+                }
+            }
+	}
+	
+	public void sortByDueDate()
+	{
+              
+	}
+	public void sortByTaskName()
+	{
+            
+	}
+	public void sortByPriority()
+	{
 		
 	}
+	
 	public boolean addItem(int priority, String jobDesc, String dueDate, String desc)
 	{
 		toDoItem nextItem=new toDoItem (priority, jobDesc, dueDate, desc);
 		boolean alreadyInList=false;
 		boolean conflictingPriority=false;
 
-		if(head==null)
-		{
-			head=nextItem;
-			head.next=null;
-		}
-		else
-		{
-			toDoItem n=head;
-			while(n.next!=null)
-			{
-				if(n.getjobDesc()==jobDesc)
-				{
-					alreadyInList=true;
-
-				}
-				if(n.getPriority()==priority)
-				{
-					conflictingPriority=true;
-				}
-					n=n.next;
-			}
-			if(n.getjobDesc()==jobDesc)
-			{
-				alreadyInList=true;
-			}
-			if(n.getPriority()==priority)
-			{
-				conflictingPriority=true;
-			}
-			if(!alreadyInList && !conflictingPriority)
-			{
-				n.next=nextItem;
-				n.next.next=null;
-				this.numberOfItems+=1;
-				
-				return true;
-			}
-		}
-		return false;
+		for(int i=0; i<numberOfItems; i++)
+                {
+                    if((list.get(i).getDesc()==desc)||(list.get(i).getPriority()==priority))
+                    {
+                        return false;
+                    }
+                }
+                this.numberOfItems+=1;
+                list.add(nextItem);
+                return true;
 	}
-	
+
 	public String getListofItems()
 	{
 		String listStr="";
-		toDoItem sorted=this.head;
-		while(sorted!=null)
+		for(int i=0; i<list.size(); i++)
 		{
-                    listStr+="\n";
-				listStr+=sorted.printInfo();
-				
-                                sorted=sorted.next;
+                                listStr+="\n";
+				listStr+=list.get(i).printInfo();
+			
 		}
 	return listStr;
 	}
-        public String getEachItem(toDoItem head)
+        public toDoItem getEachItem(int position)
         {
-                String listStr="";
-		toDoItem sorted=head;
-                listStr+="\n";
-                listStr+=sorted.printInfoNaked();
-                return listStr;
+            toDoItem temp = new toDoItem();
+            temp=list.get(position);
+            return temp;
         }
 	/*
-	@param: changeParam: contains inforomation on which parameter to 
-	change the item by. 
+	@param: changeParam: contains inforomation on which parameter to
+	change the item by.
 	*/
-	public void changeItem(toDoItem newItem, String changeParam)
+	public void changeItem(toDoItem newItem, String changeParam )
 	{
-            /*
-		String searchBy="";
-		switch(changeParam)
-		{
-			case "D":
-			toDoItem temp = this.head();
-				while(temp!=null)
-				{
-					if(temp.getDueDate==newItem.getDueDate)
-					{
-						temp=newItem;
-					}
-					temp=temp.next;
-				}
-			case ""
-
-			
-			case default:
-				
-		}*/
+//           for(int i=0; i<numberOfItems; i++)
+//           {
+//               if(list.get(i).)
+//           }
 	}
 	public void saveList(String fileName)
 	{
             String s="";
 		try{
-			
+
 		final String dir = System.getProperty("user.dir");
                 System.out.println(dir+"\\"+fileName+".txt");
 		 File FN = new File(dir+"\\"+fileName+".txt");
                  String saveStr=this.getListofItems();
-                 
+
 		 FileOutputStream is = new FileOutputStream(FN);
-		OutputStreamWriter osw = new OutputStreamWriter(is);    
+		OutputStreamWriter osw = new OutputStreamWriter(is);
 			BufferedWriter w = new BufferedWriter(osw);
-                        toDoItem item=this.head;
+                        toDoItem item = new toDoItem();
+                        int counter=0;
                         do
                         {
-                            s=this.getEachItem(item);
-                            
-                           
+                            item=this.getEachItem(counter);
                              w.write(item.taskName);
                             w.newLine();
                              w.write(item.dueDate);
@@ -159,12 +118,11 @@ public class toDoList {
                             w.newLine();
                             w.write(Integer.toString(item.getPriority()));
                             w.newLine();
-                             w.write(item.getStatusVal());
+                            w.write(item.getStatusVal());
                             w.newLine();
-                            item=item.next;
-                            
+                           counter++;
                         }
-                        while(item!=null); 
+                        while(counter!=numberOfItems);
 			w.close();
 		}
 		//create new file and write to it here
@@ -173,7 +131,7 @@ public class toDoList {
 			System.out.println("There was a problem writing to the file");
 		}
 	}
-        
+
 	public void loadList(String fileName)
 	{
                 toDoList openedList=new toDoList();
@@ -195,13 +153,10 @@ public class toDoList {
                          }
                          catch(Exception e)
                          {
-                          System.out.println("unable to open list");                         
+                          System.out.println("unable to open list");
                          }
-                         
-                         
-    
         }
-        
+
         public void printList()
         {
             String l= this.getListofItems();
@@ -211,17 +166,15 @@ public class toDoList {
 	{
             Scanner scan = new Scanner(System.in);
 		toDoList l= new toDoList();
-		
                 l.addItem(99, "desc", "dd", "job name");
-         
-           
-	
                 l.getListofItems();
 		l.printList();
                 System.out.println("Input save file name");
                 String FN =scan.next();
                 l.saveList(FN);
                 l.loadList(FN);
+                l.printList();
+                l.deleteItem("desc");
                 l.printList();
 	}
 
